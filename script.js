d44551ce7009f45c6b926019e0e86617fd6b458c
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-19 15:46:52 +0100

    Add array punteggio al click

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-/*L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console
- con il numero della cella cliccata.*/
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+ Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe 
+ non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
+ - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+  Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri
+consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato
+ su una cella che non era una bomba.*/
 
  const zone = document.getElementById('grid');
  const button = document.getElementById('button');
  const col = 10;
  const rows = 10;
  const total = col * rows;
- let isOnly = false;
-
-console.log(isOnly);
+ const point = [];
+ let j = 1;
 
 function createCell(x){
     let cell;
@@ -23,7 +27,6 @@ function createCell(x){
 
 
 //Creazione delle celle
-if( isOnly == false){
    button.addEventListener('click',function(){
     
     for(let i=1;i<=total;i++){
@@ -32,11 +35,14 @@ if( isOnly == false){
         cell.addEventListener('click', function(){
             cell.classList.add('clicked');
             console.log(i);
+        //Aggiunta punteggio 
+            point.push(j++);
+            console.log(point);
         });
         zone.appendChild(cell);
     }
 }); 
-}
+
 
 
   
